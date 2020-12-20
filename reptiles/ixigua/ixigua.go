dbc6e80cedf3171547cbbaba7a110089e30d99ea
rockrabbit <2896865355@qq.com>
2020-12-20 17:57:36 +0800

    优化采集器

--- reptiles/ixigua/ixigua.go
+++ reptiles/ixigua/ixigua.go
@@ -1,12 +1,43 @@
 package ixigua
 
-import "gitee.com/rock_rabbit/goannie/reptiles/types"
+import (
+	"errors"
+	"regexp"
+
+	"gitee.com/rock_rabbit/goannie/reptiles/types"
+)
+
+const (
+	referer = "https://www.ixigua.com"
+	host    = "https://www.ixigua.com"
+)
+
+// SubCmdList 子命令集
+var SubCmdList types.SubCmdList
+
+func init() {
+	SubCmdList = []types.SubCmd{}
+	// 判断视频内容页
+	SubCmdList = append(SubCmdList, types.SubCmd{
+		URLRegexps: []*regexp.Regexp{
+			regexp.MustCompile(`ixigua\.com/(\d+$|\d+/$|\d+\?|\d+/\?)`),
+		},
+		Extract: insidePage,
+	})
+}
+
+func insidePage(url string, option types.Options) ([]*types.Data, error) {
+	return nil, errors.New("暂不支持此链接")
+}
 
 type reptiles struct{}
 
 // Extract 运行采集器
 func (e *reptiles) Extract(url string, option types.Options) ([]*types.Data, error) {
-	return nil, nil
+	if extract := SubCmdList.Parse(url); extract != nil {
+		return extract(url, option)
+	}
+	return nil, errors.New("暂不支持此链接")
 }
 
 // New 创建一个采集器
--- reptiles/types/types.go
+++ reptiles/types/types.go
@@ -1,6 +1,34 @@
 package types
 
-import "gitee.com/rock_rabbit/goannie/storage"
+import (
+	"regexp"
+
+	"gitee.com/rock_rabbit/goannie/storage"
+)
+
+// Extract 运行
+type Extract = func(url string, option Options) ([]*Data, error)
+
+// SubCmd 子命令
+type SubCmd struct {
+	URLRegexps []*regexp.Regexp
+	Extract    Extract
+}
+
+// SubCmdList 子命令集
+type SubCmdList []SubCmd
+
+// Parse 解析子命令
+func (s SubCmdList) Parse(url string) Extract {
+	for _, sub := range s {
+		for _, cmd := range sub.URLRegexps {
+			if cmd.MatchString(url) {
+				return sub.Extract
+			}
+		}
+	}
+	return nil
+}
 
 // Data 采集器返回的数据
 type Data struct {
